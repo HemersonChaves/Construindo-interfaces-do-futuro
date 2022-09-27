c0cb0b2396ebf9f38a45bb94ce0bc180e5acc187
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-09-27 12:58:42 -0300

    feat(_document): adicionando componentes do next Html, Head, Main e NextScript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    // resetCSS={false}
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
