7fd7dccc1b4f0a264db7ed84ff4d404527dc64a7
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-10-02 21:31:00 -0300

    refact(responsivo): Fluxo de navegação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -17,20 +18,22 @@ export default function UserList() {
                 <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
                     <Flex mb='8' justify='space-between' align='center'>
                         <Heading size='lg' fontWeight='normal'>Usuários</Heading>
-                        <Button
-                            as='a'
-                            size='sm'
-                            fontSize='sm'
-                            colorScheme='pink'
-                            leftIcon={<Icon as={RiAddLine} fontSize='20' />}
-                        >
-                            Criar novo
-                        </Button>
+                        <Link href='/users/create' passHref>
+                            <Button
+                                as='a'
+                                size='sm'
+                                fontSize='sm'
+                                colorScheme='pink'
+                                leftIcon={<Icon as={RiAddLine} fontSize='20' />}
+                            >
+                                Criar novo
+                            </Button>
+                        </Link>
                     </Flex>
                     <Table colorScheme='whiteAlpha'>
                         <Thead>
                             <Tr>
-                                <Th px={['4','4','6']} color='gray.300' width='8'>
+                                <Th px={['4', '4', '6']} color='gray.300' width='8'>
                                     <Checkbox colorScheme='pink'></Checkbox>
                                 </Th>
                                 <Th >
@@ -45,7 +48,7 @@ export default function UserList() {
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px={['4','4','6']} >
+                                <Td px={['4', '4', '6']} >
                                     <Checkbox colorScheme='pink'></Checkbox>
                                 </Td>
                                 <Td>
@@ -54,7 +57,7 @@ export default function UserList() {
                                         <Text fontSize='sm' color='gray.300'> Hemersonchaves@email.com</Text>
                                     </Box>
                                 </Td>
-                                {isWideVersion && <Td>04 de Abril, 2022</Td> }
+                                {isWideVersion && <Td>04 de Abril, 2022</Td>}
                                 <Td><Button
                                     as='a'
                                     size='sm'
@@ -66,7 +69,7 @@ export default function UserList() {
                                 </Button></Td>
                             </Tr>
                             <Tr>
-                                <Td px={['4','4','6']} >
+                                <Td px={['4', '4', '6']} >
                                     <Checkbox colorScheme='pink'></Checkbox>
                                 </Td>
                                 <Td>
@@ -75,7 +78,7 @@ export default function UserList() {
                                         <Text fontSize='sm' color='gray.300'> Hemersonchaves@email.com</Text>
                                     </Box>
                                 </Td>
-                                {isWideVersion && <Td>04 de Abril, 2022</Td> }
+                                {isWideVersion && <Td>04 de Abril, 2022</Td>}
                                 <Td><Button
                                     as='a'
                                     size='sm'
@@ -87,7 +90,7 @@ export default function UserList() {
                                 </Button></Td>
                             </Tr>
                             <Tr>
-                                <Td px={['4','4','6']} >
+                                <Td px={['4', '4', '6']} >
                                     <Checkbox colorScheme='pink'></Checkbox>
                                 </Td>
                                 <Td>
@@ -96,7 +99,7 @@ export default function UserList() {
                                         <Text fontSize='sm' color='gray.300'> Hemersonchaves@email.com</Text>
                                     </Box>
                                 </Td>
-                                {isWideVersion && <Td>04 de Abril, 2022</Td> }
+                                {isWideVersion && <Td>04 de Abril, 2022</Td>}
                                 <Td><Button
                                     as='a'
                                     size='sm'
