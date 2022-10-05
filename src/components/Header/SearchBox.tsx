2d39cd2213e35c2dd2c369faabca72bf1575715c
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-10-05 12:53:57 -0300

    chore(form): adiciona react-hook-form

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,7 +1,15 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
 export function SearchBox() {
+  // const [search, setSearch] = useState('')
+  // debounce - Controlled components
+  
+  // uncontrolled components
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  console.log(searchInputRef.current.value)
   return (
     <Flex
       as='label'
@@ -23,6 +31,9 @@ export function SearchBox() {
         mr='4'
         placeholder='Buscar na plataforma'
         _placeholder={{ color: 'gray.400' }}
+        // value={search}
+        // onChange={event => setSearch(event.target.value)}
+        ref={searchInputRef}
       />
       <Icon as={RiSearchLine} fontSize='20' />
     </Flex>
